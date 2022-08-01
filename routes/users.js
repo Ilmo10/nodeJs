69643c763269c9f94c2bcf43aa946aa6afb729ab
Ilmo10 <necok_cley@hotmail.com>
2022-08-01 09:28:28 -0300

    listando usuario nedb

--- routes/users.js
+++ routes/users.js
@@ -10,16 +10,22 @@ module.exports = (app)=>{
 
 app.get('/users',(req, res) => {
 
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'application/json')
-    res.json({
-        users:[{
-            name: 'Hcode',
-            email: 'contato@hcode.com.br',
-            id: 1
-        }]
-    });
+    db.find({}).sort({name:1}).exec((err, users)=>{
 
+        if (err) {
+            console.log(`error: ${err}`);
+            res.status(400).json();
+        } else {
+
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'application/json')
+            res.json({
+                users
+            });
+
+        }
+
+    });
 });
 
 app.post('/users',(req, res) => {
@@ -28,7 +34,7 @@ app.post('/users',(req, res) => {
 
         if(err) {
 
-            console.log(`error:, ${err}`);
+            console.log(`error: ${err}`);
             res.status(400).json({
                 error:err
             });
